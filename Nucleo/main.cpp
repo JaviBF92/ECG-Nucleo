c5ffb15b8ce58e5a76736b5cd890ca5d1e20621f
JaviBF92 <javibf1992@gmail.com>
2016-10-31 18:33:39 +0100

    Añadida funcion de filtro de media; frecuencia de muestreo alterada

--- Nucleo/main.cpp
+++ Nucleo/main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-#include <list>
 
 AnalogIn shield(PC_1);
 Serial pc(USBTX, USBRX);
@@ -8,15 +7,54 @@ PwmOut cal(D9);
 Ticker ticker;
 
 char myCharPointer[6];
+int i;
+
+float buffer[5] = { };
+int ind;
+bool firstValues;
+
+const float fEnvio = 0.025;			//envio 40 Hz
+const float fMuestreo = 0.00625;	//muestreo 160 hz
+
+float medianFilter(float value){
+
+	float v = 0;
+	buffer[ind] = value;
+
+	if(ind >= 4){
+		ind = 0;
+
+		if(firstValues){
+			firstValues = false;
+		}
+	}
+
+	if(!firstValues){
+		for(int j = 0; j < 5; j ++){
+			v += buffer[j];
+		}
+
+		v = v / 5;
+	}
+
+	ind ++;
+	return v;
+}
 
 void send(){
 	float val = shield.read();
+	val = medianFilter(val);
+	float f = (fEnvio / fMuestreo) - 1;
 
-	sprintf(myCharPointer, "{%1.2f}", val);
-	pc.printf(myCharPointer);
-	bluetooth.printf(myCharPointer);
-	memset(myCharPointer, 0, sizeof myCharPointer);
+	if(i >= f){
+		sprintf(myCharPointer, "{%1.2f}", val);
+		pc.printf(myCharPointer);
+		bluetooth.printf(myCharPointer);
+		memset(myCharPointer, 0, sizeof myCharPointer);
+		i = 0;
+	}
 
+	i ++;
 }
 
 int main() {
@@ -25,8 +63,11 @@ int main() {
 	cal.period(0.1f);
 	cal.write(0.50f);
 
-	ticker.attach(&send, 0.025);
+	ind = 0;
+	firstValues = true;
+	i = 0;
+
+	ticker.attach(&send, fMuestreo);
 
 	while(1){}
 }
-
